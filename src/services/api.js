6847c6f8756bf1e924e1387f05b5c68be1c1bda3
Silas Silva <silas.silva@liferay.com>
2023-10-19 09:33:20 -0300

    Remove sensitive information

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const token = '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';
+const token = '';
 const api = axios.create({
     baseURL: 'https://api.github.com/',
     headers: {'Authorization': 'Bearer '+token} 
